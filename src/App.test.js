aed88ab1fcb05de9a693c0f29fa414ea8fd59cd1
Will Lorand <will@milemarker3.org>
2020-12-01 10:49:42 -0800

    cra cleanup + rendered test table with useState hook

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
+
+// testable component
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders React Table Placeholder Posts text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/React Table Placeholder Posts/i);
+  expect(headerElement).toBeInTheDocument();
 });
