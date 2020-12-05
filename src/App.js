cfda9fae0a8246dafa82517479fa6c2b73b60be4
Will Lorand <will@milemarker3.org>
2020-12-05 14:43:50 -0800

    rendered first react table, downgraded to v6

--- src/App.js
+++ src/App.js
@@ -1,35 +1,52 @@
 // import React from 'react'; // not needed in v17
 import { useState, useEffect } from 'react';
 
+// data fetching libs
 import axios from 'axios';
 
+// data rendering libs
+// import { useState } from 'react-table'; // v7
+import ReactTable from 'react-table'; // v6
+
 // styles
+import 'react-table/react-table.css'; // v6 only - this throws not found error in v7
 import './styles/app.css';
 
 function App() {
-  const [posts] = useState([1, 2, 3]);
+  // TODO: move to services/fetchApiData.js and import here
+  const [posts, setPosts] = useState([]);
   const url = 'https://jsonplaceholder.typicode.com/posts';
 
   // useEffect as mimick of componentDidMount
   // -- use it here first w/o useState for starters (likely storing in state next)
   useEffect(() => {
     // fetch with axios and log data
+    // -- todo: put this in a services file and invoke from there
     const fetchData = async (query) => {
       const result = await axios.get(query);
-      console.log(result.data); // axios result object also has status and lots more
+      console.log(result.data instanceof Array); // axios result object also has status and lots more
+      // store in state
+      setPosts(result.data);
     };
     // invoke
     fetchData(url);
   }, []); // empty dependency array means just runs once onLoad aka componentDidMount
+
+  // define table columns
+  const columns = [
+    { Header: 'User ID', accessor: 'userId' },
+    { Header: 'ID', accessor: 'id' },
+    { Header: 'Title', accessor: 'title' },
+    { Header: 'Content', accessor: 'body' },
+  ];
 
   return (
     <div>
       <header className="app-header">
         <h1>React Table Placeholder Posts</h1>
       </header>
-      {posts.map((post, i) => (
-        <p key={i}>{post}</p>
-      ))}
+      {/* TODO: move the ReactTable to its own component */}
+      <ReactTable columns={columns} data={posts} />
     </div>
   );
 }
